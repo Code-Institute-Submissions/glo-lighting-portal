84e0710bc8b312bf73464b655e21aed15b36c352
Steven John Bolton <steviebolton@gmail.com>
2018-09-05 09:04:52 +0000

    edit word Lamp

--- cart/utils.py
+++ cart/utils.py
@@ -5,11 +5,11 @@ from products.models import Lamp
 def get_cart_items_and_total(cart):
     total = 0
     cart_items = []
-    for lamp_id, quantity in cart.items():
-        lamp = get_object_or_404(Lamp, pk=lamp_id)
-        item_total = quantity * lamp.bruto_price
+    for product_id, quantity in cart.items():
+        product = get_object_or_404(Lamp, pk=lamp_id)
+        item_total = quantity * product.bruto_price
         total += item_total
-        cart_items.append({'lamp':lamp, 'quantity': quantity, 'total': item_total}) 
+        cart_items.append({'product':product, 'quantity': quantity, 'total': item_total}) 
         
         
     return {'cart_items' : cart_items, 'total': total}
