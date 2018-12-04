e6deca1e1bd4f8cfd81a59ccab00116c388cc2cc
Steven John Bolton <steviebolton@gmail.com>
2018-12-04 10:22:17 +0000

    blog/artical image added to artical_list

--- articles/models.py
+++ articles/models.py
@@ -13,7 +13,7 @@ class Article(models.Model):
     views = models.IntegerField(default=0)
     tag = models.CharField(max_length=30, blank=True, null=True)
     author = models.ForeignKey(User, related_name='article', null=False, default=1, on_delete=models.SET_DEFAULT) 
-    image = models.ImageField(upload_to="images" , null=True, blank=True)
+    image = models.ImageField(upload_to="article-images" , null=True, blank=True)
 def __str__(self):
     return self.title
     
