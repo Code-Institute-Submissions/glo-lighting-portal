3e6ddd0b27d1b6452a721f02e6ab1bc9d1b1f128
Steven John Bolton <steviebolton@gmail.com>
2018-12-04 09:33:05 +0000

    update articles db

--- articles/models.py
+++ articles/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Article(models.Model):
@@ -7,7 +9,12 @@ class Article(models.Model):
     slug = models.SlugField()
     body = models. TextField()
     date = models.DateTimeField(auto_now_add=True)
-    # add in thumbnail later
-    # add in author later
+    published_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
+    views = models.IntegerField(default=0)
+    tag = models.CharField(max_length=30, blank=True, null=True)
+    author = models.ForeignKey(User, related_name='article', null=False, default=1, on_delete=models.SET_DEFAULT) 
+    image = models.ImageField(upload_to="images" , null=True, blank=True)
 def __str__(self):
     return self.title
+    
+    
